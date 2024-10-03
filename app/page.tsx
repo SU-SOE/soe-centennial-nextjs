caa1d12d5225e3a81b64da100006154f2f03f337
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-02 18:37:31 -0700

    SOEOPSFY24-202 |

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { Heading } from './components/Typography';
+import { Heading } from '@/components/Typography';
+import { ShapeAnimation } from './components/ShapeAnimation';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <Heading>SOE Centennial</Heading>
+    <div>
+      <Heading>SOE Centennial</Heading>
+      <ShapeAnimation />
     </div>
   );
 }
