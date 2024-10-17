2b9e9c1df262741d7f2e93d442153d6dfc32b474
rebeccahongsf <rebeccahongsf@users.noreply.github.com>
2024-10-17 04:33:00 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/app/page.tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@/components/Typography';
+import { ShapeAnimation } from './components/ShapeAnimation';
+
+export default function Home() {
+  return (
+    <div>
+      <Heading>SOE Centennial</Heading>
+      <ShapeAnimation />
+    </div>
+  );
+}
