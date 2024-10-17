cfd26540e7202130b3c8c5bea983c26ce39c0bc5
rebeccahongsf <rebeccahongsf@users.noreply.github.com>
2024-10-17 04:33:14 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/app/page.tsx
@@ -0,0 +1,9 @@
+import { TimelineBanner } from './components/TimelineBanner';
+
+export default function Home() {
+  return (
+    <div>
+      <TimelineBanner heading='Headline lorem ipsum dolar sit' superhead='1983' body="Cras felis tortor, bibendum quis porttitor at, ullamcorper eu diam. Aenean iaculis efficitur quam, quis dignissim lacus. Cras nec dui id mi blandit lobortis eget in enim." />
+    </div>
+  );
+}
