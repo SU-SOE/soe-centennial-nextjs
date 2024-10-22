ef0df522c378aeee038dfa6a89f38046ac9d2ac7
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-21 21:49:47 -0700

    SOEOPSFY24-296 | explore framer motion hero multi-overlay

---
+++ app/components/FeatureHero/FeatureHero.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { HTMLAttributes } from 'react';
+import { Container } from '../Container';
+import { FlexBox } from '../FlexBox';
+// import * as styles from './FeatureHero.styles';
+import Image from 'next/image';
+import ShapeA from '../images/shape-a';
+import { motion, useScroll, useTransform } from "framer-motion";
+
+type FeatureHeroProps = HTMLAttributes<HTMLDivElement> & {
+  children: React.ReactNode;
+  imageSrc?: string;
+};
+
+export const FeatureHero = ({
+  children,
+  ...props
+}: FeatureHeroProps) => {
+  const { scrollYProgress } = useScroll();
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
+
+
+  return (
+    <FlexBox {...props} as="section" alignItems="start" justifyContent="between" className="flex-col bg-soe-black">
+    <Container width="full" className='order-last flex flex-col rs-p-9 *:text-white z-50 lg:max-w-[50%] *:font-dm-sans bg-soe-black lg:bg-transparent'>
+      {children}
+    </Container>
+    <div className="relative aspect-[16/9] lg:aspect-auto w-full bg-cool-grey lg:absolute lg:top-0 lg:h-full overflow-hidden z-10">
+      <Image className="absolute top-0 left-0 lg:left-[300px] z-0"
+        alt=""
+        src={`soe-centennial-nextjs/assets/images/Hawa-Racine-Thiam.jpg`}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: '120%', height: 'auto' }} />
+      <motion.div className="absolute w-full h-full top-0 left-0 z-10" style={{ opacity }} >
+        <div className="w-full h-full bg-plum-dark absolute top-0 left-0 z-10" />
+        <ShapeA className="absolute bottom-10 left-10 lg:left-[300px] z-20 *:stroke-plum" height={1200} />
+      </motion.div>
+      <div className='absolute top-0 left-0 bg-transparent lg:bg-feature-gradient-rl bg-feature-gradient-bt w-full h-full z-50' />
+      <Image className="absolute top-0 left-0 lg:left-[300px] z-20"
+        alt=""
+        src={`soe-centennial-nextjs/assets/images/Hawa-Racine-Thiam_silhouette.png`}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: '120%', height: 'auto' }} />
+      <div className='absolute top-0 left-0 bg-feature-gradient-bt w-full h-full z-50' />
+    </div>
+  </FlexBox>
+)};
---
+++ app/components/images/HeroIcon/HeroIcon.styles.tsx
@@ -0,0 +1,89 @@
+import {
+  ArrowPathIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+  ArrowDownTrayIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowUpRightIcon,
+  Bars3Icon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+  CursorArrowRaysIcon,
+  DocumentDuplicateIcon,
+  EnvelopeIcon,
+  LinkIcon,
+  MapPinIcon,
+  MinusIcon,
+  PauseCircleIcon,
+  PlayCircleIcon,
+  PlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline';
+import { CheckIcon } from '@heroicons/react/16/solid';
+import { PlayIcon } from '@heroicons/react/20/solid';
+
+export const iconMap = {
+  action: ChevronRightIcon,
+  'arrow-right': ArrowRightIcon,
+  'arrow-left': ArrowLeftIcon,
+  'arrow-up': ArrowUpIcon,
+  'arrow-down': ArrowDownIcon,
+  back: ArrowLeftIcon,
+  copy: DocumentDuplicateIcon,
+  check: CheckIcon,
+  'chevron-down': ChevronDownIcon,
+  'chevron-right': ChevronRightIcon,
+  'chevron-up': ChevronUpIcon,
+  download: ArrowDownTrayIcon,
+  'triangle-down': PlayIcon,
+  'triangle-right': PlayIcon,
+  'triangle-up': PlayIcon,
+  cursor: CursorArrowRaysIcon,
+  close: XMarkIcon,
+  email: EnvelopeIcon,
+  external: ArrowUpRightIcon,
+  flip: ArrowPathIcon,
+  left: ArrowLeftIcon,
+  link: LinkIcon,
+  location: MapPinIcon,
+  menu: Bars3Icon,
+  minus: MinusIcon,
+  more: ArrowRightIcon,
+  pause: PauseCircleIcon,
+  play: PlayIcon,
+  'play-outline': PlayCircleIcon,
+  plus: PlusIcon,
+  right: ArrowRightIcon,
+  up: ArrowUpIcon,
+};
+export type IconType = keyof typeof iconMap;
+
+/**
+ * Normalized base size and position of each icon (finetuned manually) for use in eg, buttons
+ * Only add to this map if different from default class w-1em
+ * If you wish to use the HeroIcon without any base styles, set the noBaseStyle boolean prop to true
+ */
+
+// This basically means that the keys from iconBaseStyle are from the keys of iconMap
+type IconBaseStyleType = Partial<{
+  [Key in IconType]: string;
+}>;
+
+export const iconBaseStyleDefault = 'w-1em';
+export const iconBaseStyle: IconBaseStyleType = {
+  'arrow-left': 'w-09em -mt-01em',
+  'arrow-right': 'w-09em -mt-01em',
+  'triangle-right': 'w-09em scale-x-90 mt-01em',
+  'triangle-down': 'w-09em scale-x-90 rotate-90 mt-01em',
+  'triangle-up': 'w-09em scale-x-90 -rotate-90 mt-02em',
+  download: 'w-09em',
+  email: 'w-1em',
+  external: 'w-08em stroke-[2.5]',
+  left: 'w-08em',
+  link: 'w-09em -mt-01em',
+  more: 'w-08em',
+  plus: 'w-08em',
+  right: 'w-08em',
+};
