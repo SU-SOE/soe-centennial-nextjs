e91458a98da6254ba2dc8e36fbe023e82aacc276
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-29 19:48:22 -0700

    add font

--- app/components/TimelineBanner/TimelineBanner.styles.ts
+++ app/components/TimelineBanner/TimelineBanner.styles.ts
@@ -1,6 +1,6 @@
 export const root = 'overflow-hidden';
 export const wrapper = 'mr-0 au-ml-auto flex-col lg:flex-row';
-export const contentWrapper = 'lg:rs-pr-9 ml-0 flex flex-col';
+export const contentWrapper = 'lg:rs-pr-9 ml-0 flex flex-col *:font-dm-sans';
 export const heading = '2xl:whitespace-pre-line -mt-01em rs-mb-2 xl:max-w-1200';
 export const superhead = 'order-first mb-38';
 export const dek = 'max-w-[50ch] type-3 rs-mb-3 *:*:leading-snug';
