03d82889cbefd9f87d6dd5ec551440aa2ac8a090
rebeccahongsf <rebeccahongsf@gmail.com>
2024-11-19 21:04:09 -0800

    build timeline image component

---
+++ app/components/Timeline/TimelineImage.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { cnb } from "cnbuilder";
+import * as styles from "./TimelineItem.styles";
+import * as types from "./TimelineItem.types";
+
+interface TimelineImageProps {
+  src: string;
+  alt?: string;
+  size?: types.SizeType;
+  trapezoidAngle?: types.TrapezoidType;
+  className?: string;
+}
+
+export const TimelineImage = ({
+  src,
+  alt = "",
+  size = "medium",
+  trapezoidAngle = "left",
+  className,
+}: TimelineImageProps) => {
+  const imageSize = styles.size[size];
+  const trapezoidType = styles.trapezoid[trapezoidAngle];
+
+  return (
+    <div className={cnb("group relative", trapezoidType)}>
+      <div
+        className={cnb(
+          "aspect-[1/1] relative perspective-600 group-hocus:scale-105 transform ease-in-out duration-1000",
+          imageSize,
+          className,
+        )}
+      >
+        <Image
+          alt={alt}
+          src={src}
+          fill
+          className="object-cover rounded-lg shadow-lg transform ease-in-out duration-1000"
+        />
+      </div>
+    </div>
+  );
+};
