54862bdc9618fb6bd664e1537aece33ebf2edc40
rebeccahongsf <rebeccahongsf@gmail.com>
2024-11-21 11:24:59 -0800

    restructure directory

--- out/_next/static/chunks/app/timeline/page-7af646bae743745b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[571],{127:(e,s,n)=>{Promise.resolve().then(n.bind(n,9811)),Promise.resolve().then(n.bind(n,5978)),Promise.resolve().then(n.t.bind(n,231,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[143,980,804,55,130,215,744],()=>s(127)),_N_E=e.O()}]);
