ddd3365bfb9a0bd4687437e4d9d683a50101f61a
rebeccahongsf <rebeccahongsf@gmail.com>
2025-02-10 11:03:11 -0800

    SOEOPSFY24 | set bot indexing to false

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,12 +65,12 @@ export const metadata = {
     apple: appleIcons,
   },
   robots: {
-    index: true,
-    follow: true,
+    index: false,
+    follow: false,
     nocache: false,
     googleBot: {
-      index: true,
-      follow: true,
+      index: false,
+      follow: false,
       noimageindex: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
