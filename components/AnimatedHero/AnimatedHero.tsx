24c0d74252f20519a5be670bd81fc60f042f8215
rebeccahongsf <rebeccahongsf@gmail.com>
2025-02-18 12:29:09 -0800

    add transitions for images

--- components/AnimatedHero/AnimatedHero.tsx
+++ components/AnimatedHero/AnimatedHero.tsx
@@ -25,50 +25,8 @@ const AnimatedHero = () => {
         </AnimateInView>
       )}
       <AnimateInView animation="fadeIn" duration={0.8}>
-        <div className="relative w-screen h-screen flex flex-row items-center justify-center cc">
-          <div className="relative w-1/3">
-            <motion.div
-              key="robot"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: [0, 1, 1, 0] }}
-              transition={{
-                delay: 2.25,
-                duration: 3,
-                times: [0, 0.2, 0.8, 1],
-                ease: "easeInOut",
-              }}
-            >
-              <Image
-                alt=""
-                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481945/robotFlipped_e0kkk6.png"
-                width="645"
-                height="406"
-                className="absolute top-0"
-              />
-            </motion.div>
-            <motion.div
-              key="ship"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: [0, 1, 1, 0] }}
-              transition={{
-                delay: 8,
-                duration: 3,
-                times: [0, 0.2, 0.8, 1],
-                ease: "easeInOut",
-              }}
-              className="flex justify-center items-center"
-            >
-              <Image
-                alt=""
-                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481946/130704-9205_silhouette_j9uj3j.png"
-                width="686"
-                height="210"
-                className="absolute top-0"
-              />
-            </motion.div>
-            {/* <LineAnimation order={[0, 0, 0, 0, 1, 7, 3, 0]} /> */}
-          </div>
-          <div className="relative w-1/3">
+        <div className=" w-screen h-screen flex flex-row items-center justify-center cc">
+          <div className="relative w-1/3 h-600">
             <motion.div
               key="william-hewlett"
               initial={{ opacity: 0 }}
@@ -88,6 +46,48 @@ const AnimatedHero = () => {
                 className="absolute top-0"
               />
             </motion.div>
+            <motion.div
+              key="robot"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 1, 1, 0] }}
+              transition={{
+                delay: 5,
+                duration: 3,
+                times: [0, 0.2, 0.8, 1],
+                ease: "easeInOut",
+              }}
+            >
+              <Image
+                alt=""
+                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481945/robotFlipped_e0kkk6.png"
+                width="645"
+                height="406"
+                className="absolute top-0"
+              />
+            </motion.div>
+            <motion.div
+              key="ship"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 1, 1, 0] }}
+              transition={{
+                delay: 7.5,
+                duration: 3,
+                times: [0, 0.2, 0.8, 1],
+                ease: "easeInOut",
+              }}
+              className="flex justify-center items-center"
+            >
+              <Image
+                alt=""
+                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481946/130704-9205_silhouette_j9uj3j.png"
+                width="686"
+                height="210"
+                className="absolute top-0"
+              />
+            </motion.div>
+            {/* <LineAnimation order={[0, 0, 0, 0, 1, 7, 3, 0]} /> */}
+          </div>
+          <div className="relative w-1/3 h-600">
             <motion.div
               key="irmgard"
               initial={{ opacity: 0 }}
@@ -112,7 +112,7 @@ const AnimatedHero = () => {
               initial={{ opacity: 0 }}
               animate={{ opacity: [0, 1, 1, 0] }}
               transition={{
-                delay: 6,
+                delay: 7,
                 duration: 3,
                 times: [0, 0.2, 0.8, 1],
                 ease: "easeInOut",
@@ -120,7 +120,7 @@ const AnimatedHero = () => {
             >
               <Image
                 alt=""
-                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481951/irmgardFluggeLotz_silhouette_gfjjgm.png"
+                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481947/maeJemison_crwmks.png"
                 width="538"
                 height="624"
                 className="absolute top-0"
@@ -133,7 +133,7 @@ const AnimatedHero = () => {
               }}
             /> */}
           </div>
-          <div className="relative w-1/3">
+          <div className="relative w-1/3 h-600">
             <motion.div
               key="william-hewlett"
               initial={{ opacity: 0 }}
@@ -154,7 +154,27 @@ const AnimatedHero = () => {
               />
             </motion.div>
             <motion.div
-              key="irmgard"
+              key="machine"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 1, 1, 0] }}
+              transition={{
+                delay: 6,
+                duration: 3,
+                times: [0, 0.2, 0.8, 1],
+                ease: "easeInOut",
+              }}
+              className="flex justify-center items-center"
+            >
+              <Image
+                alt=""
+                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481947/machineSilhouette_xnmpup.png"
+                width="280"
+                height="760"
+                className="absolute top-0"
+              />
+            </motion.div>
+            <motion.div
+              key="group"
               initial={{ opacity: 0 }}
               animate={{ opacity: [0, 1, 1, 0] }}
               transition={{
@@ -167,7 +187,7 @@ const AnimatedHero = () => {
             >
               <Image
                 alt=""
-                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481947/machineSilhouette_xnmpup.png"
+                src="https://res.cloudinary.com/dsqi5touf/image/upload/v1739481944/group_silhouette_wmmyl3.png"
                 width="280"
                 height="760"
                 className="absolute top-0"
