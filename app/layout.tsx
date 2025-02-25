d590bef564c5f985c5de397010d82c85a3999535
rebeccahongsf <rebeccahongsf@gmail.com>
2025-02-24 16:00:29 -0800

    SOEOPSFY24-482 | Configure Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import localFont from "next/font/local";
 import { Source_Sans_3, DM_Sans, DM_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { cnb } from "cnbuilder";
 import { Icon } from "next/dist/lib/metadata/types/metadata-types";
 import { GlobalFooter } from "@/components/GlobalFooter";
@@ -102,6 +103,7 @@ export default function RootLayout({
           <GlobalFooter />
         </footer>
       </body>
+      <GoogleAnalytics gaId="G-Z9E6WWH5DJ" />
     </html>
   );
 }
