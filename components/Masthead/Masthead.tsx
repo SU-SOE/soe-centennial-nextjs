a0bba2171c0402816b2d29812ca3e04c4cd61ef4
rebeccahongsf <rebeccahongsf@gmail.com>
2025-02-25 16:16:48 -0800

    SOEOPSFY24-430 | add tagline animation

--- components/Masthead/Masthead.tsx
+++ components/Masthead/Masthead.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { HTMLAttributes } from "react";
 import { cnb } from "cnbuilder";
 import { FlexBox } from "@/components/FlexBox";
@@ -5,7 +7,7 @@ import { Skiplink } from "@/components/SkipLink";
 import * as styles from "./Masthead.styles";
 import { EngLogoLockup } from "../Logo/EngLogoLockup";
 // import { MainNav } from "@/components/MainNav";
-import { Text } from "@/components/Typography";
+import { motion } from "motion/react";
 
 export type MastheadProps = HTMLAttributes<HTMLDivElement> & {
   logoColor?: "default" | "white" | "white-red";
@@ -46,19 +48,29 @@ export const Masthead = ({
       >
         <div className={styles.logo}>
           <EngLogoLockup color={logoColor} isLink />
-          <Text
-            font="dm-sans"
-            size="small"
-            className="mt-10 md:mt-18 max-w-200"
-            mb="0"
-            leading="snug"
+          <div className="font-dm-sans text-16 leading-snug block lg:hidden max-w-200">
+            A century of discovery, innovation, and impact
+          </div>
+        </div>
+        <div className="hidden lg:flex flex-row justify-center items-center w-full">
+          <motion.div
+            initial={{ fontSize: "4.6rem", width: "500px" }}
+            whileInView={{
+              fontSize: "1.6rem",
+              width: "200px",
+              transition: { duration: 1, delay: 0.8 },
+            }}
+            viewport={{ once: true, amount: "all" }}
+            className="font-dm-sans leading-snug"
           >
             A century of discovery, innovation, and impact
-          </Text>
+          </motion.div>
         </div>
-        <FlexBox alignItems="center" className={styles.flexbox}>
-          {mainNav}
-        </FlexBox>
+        {mainNav && (
+          <FlexBox alignItems="center" className={styles.flexbox}>
+            {mainNav}
+          </FlexBox>
+        )}
       </FlexBox>
     </header>
   );
