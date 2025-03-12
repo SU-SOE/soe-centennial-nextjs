4aef809140e393ac9df9362dd360a297da6d1aeb
rebeccahongsf <rebeccahongsf@gmail.com>
2025-03-12 14:20:08 -0700

    document masthead

--- components/Masthead/Masthead.tsx
+++ components/Masthead/Masthead.tsx
@@ -1,3 +1,22 @@
+/**
+ * Props for the Masthead component.
+ *
+ * @typedef {Object} MastheadProps
+ * @property {"default" | "white" | "white-red"} [logoColor] - The color of the logo.
+ * @property {boolean} [isOverlap=false] - Whether the masthead overlaps content.
+ * @property {boolean} [navLight] - Whether the navigation is light.
+ * @property {boolean} [isBgDark=false] - Whether the background is dark.
+ * @property {styles.BgColorType} [bgColor="none"] - The background color type.
+ * @property {React.ReactNode} [mainNav] - The main navigation component.
+ * @property {string} [className] - Additional class names for the masthead.
+ */
+
+/**
+ * Masthead component for displaying the header section of the page.
+ *
+ * @param {MastheadProps} props - The props for the Masthead component.
+ * @returns {JSX.Element} The rendered Masthead component.
+ */
 import { HTMLAttributes } from "react";
 import { cnb } from "cnbuilder";
 import { FlexBox } from "@/components/FlexBox";
