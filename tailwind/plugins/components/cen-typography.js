59149752fabe4e327cfbb742163f3b3526b7f70d
rebeccahongsf <rebeccahongsf@gmail.com>
2025-03-18 10:39:33 -0700

    fixup overview line height

--- tailwind/plugins/components/cen-typography.js
+++ tailwind/plugins/components/cen-typography.js
@@ -6,7 +6,7 @@ module.exports = function () {
     const components = {
       '.overview': {
         ...theme('decanter.typography.type1'),
-        lineHeight: '1.9rem',
+        lineHeight: '1.9em',
         fontWeight: theme('fontWeight.normal'),
       },
       '.cen-big-paragraph': {
