8957a581bdca32ade824704245bcf2dac2df235c
rebeccahongsf <rebeccahongsf@gmail.com>
2025-03-18 18:00:17 -0700

    create ch2 page

---
+++ app/stories/the-foundation-of-a-new-era-in-engineering/page.tsx
@@ -0,0 +1,22 @@
+// import { Container } from "@/components/Container";
+// import { Masthead } from "@/components/Masthead";
+// import { StoryImpactBanner } from "@/components/Story";
+
+// export default function page() {
+//   return (
+//     <Container as="article" width="full">
+//       <Masthead logoColor="white-red" isOverlap isBgDark />
+//       <StoryImpactBanner
+//         hasBgImage
+//         bgColor="stone-dark"
+//         chapter="Decade 1"
+//         heading="“Preparing Roads over which Culture May Ride in Comfort”"
+//         dek="1925 – 1934"
+//         bgImageSrc="https://res.cloudinary.com/duv7bozlj/image/upload/v1741726629/high_voltage_laboratory_oyh87g.jpg"
+//         src="https://res.cloudinary.com/duv7bozlj/image/upload/v1742342467/Ryan-High-Voltage-Laboratory_927_tighter_Chapter-feature-image_mccqgm.jpg"
+//         alt="The southeast corner of Stanford’s main quad, known as “Engineering Corner,” circa 1910–1915"
+//         caption="The Harris J. Ryan High-Voltage Laboratory debuted on September 17, 1926, with a public demonstration of its 2,100,000-volt test station containing six 350,000-volt transformer units, capable of producing the highest voltage ever obtained at commercial frequency. Erected at a cost of nearly $500,000, it was the largest university electrical lab in existence at the time. | Berton W. Crandall photographs, Box 24, Hoover Institution Library & Archives."
+//       />
+//     </Container>
+//   );
+// }
