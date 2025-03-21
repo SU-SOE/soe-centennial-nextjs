bd0add9bd99722da9729c84db63f4e2e36c62fa1
rebeccahongsf <rebeccahongsf@gmail.com>
2025-03-21 14:41:59 -0700

    swapout chapter 1 image

--- app/stories/preparing-roads-culture-may-ride-comfort/page.tsx
+++ app/stories/preparing-roads-culture-may-ride-comfort/page.tsx
@@ -186,7 +186,7 @@ export default function page() {
             heading="“Preparing Roads over which Culture May Ride in Comfort”"
             dek="1925 – 1934"
             bgImageSrc="https://res.cloudinary.com/duv7bozlj/image/upload/v1741726629/high_voltage_laboratory_oyh87g.jpg"
-            src="https://res.cloudinary.com/duv7bozlj/image/upload/v1742342467/Ryan-High-Voltage-Laboratory_927_tighter_Chapter-feature-image_mccqgm.jpg"
+            src="https://res.cloudinary.com/duv7bozlj/image/upload/v1741726629/high_voltage_laboratory_oyh87g.jpg"
             alt="The southeast corner of Stanford’s main quad, known as “Engineering Corner,” circa 1910–1915"
             caption="The Harris J. Ryan High-Voltage Laboratory debuted on September 17, 1926, with a public demonstration of its 2,100,000-volt test station containing six 350,000-volt transformer units, capable of producing the highest voltage ever obtained at commercial frequency. Erected at a cost of nearly $500,000, it was the largest university electrical lab in existence at the time. | Berton W. Crandall photographs, Box 24, Hoover Institution Library & Archives."
           />
