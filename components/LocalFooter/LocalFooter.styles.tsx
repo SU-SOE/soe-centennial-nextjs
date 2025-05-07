0b3474c960e3b25af4ef9b7e608daedd21434e1b
rebeccahongsf <rebeccahongsf@gmail.com>
2025-05-06 19:14:49 -0700

    adjust spacing

--- components/LocalFooter/LocalFooter.styles.tsx
+++ components/LocalFooter/LocalFooter.styles.tsx
@@ -3,7 +3,7 @@ export const flexWrapper =
 export const logo =
   "max-w-[32.5rem] flex flex-col items-start justify-between h-full";
 export const contentWrapper =
-  "flex-col md:flex-row shrink-0 w-full gap-95 justify-between";
+  "flex-col md:flex-row shrink-0 w-full gap-45 md:gap-95 justify-between";
 export const column = "w-full md:w-1/2 max-w-500";
 export const wrapper = "gap-45 md:gap-95 w-full max-w-900";
 export const linksWrapper = "flex-col md:flex-row w-full gap-45 md:gap-95";
