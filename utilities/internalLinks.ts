956b70f60f672b2377cece37b5e29f2cb634bce7
rebeccahongsf <rebeccahongsf@gmail.com>
2025-05-29 08:30:01 -0700

    SOECENT | add internal links file

---
+++ utilities/internalLinks.ts
@@ -0,0 +1,20 @@
+/**
+ * Central place for commonly used internal links
+ * Esp for links that are used in multiple places and might change URL in the future
+ */
+
+export const internalLinks = {
+  home: "/",
+  events: "/events",
+  stories: "/stories",
+  deanWidomLetter: "/letter-from-dean-widom",
+  storyInnovation: "/stories/a-century-of-innovation",
+  decade1: "/stories/preparing-roads-culture-may-ride-comfort",
+  decade2: "/stories/the-foundation-of-a-new-era-in-engineering",
+  decade3: "/stories/the-terman-era",
+  decade4: "/stories/a-period-of-transformation",
+  decade5: "/stories/new-spaces-and-expanding-influence",
+  decade6: "/stories/bridging-worlds-chips-code-and-cosmos",
+  decade7: "/stories/redevelopment-and-an-earthquake",
+  decade8: "/",
+} as const;
