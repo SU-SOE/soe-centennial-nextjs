38e2ef36f1258060c3a3ba6e3089502c72cbf267
rebeccahongsf <rebeccahongsf@gmail.com>
2025-06-23 15:35:06 -0700

    SOECENT | adjust stories nav for accessibility

--- components/StoriesNav/StoriesNav.tsx
+++ components/StoriesNav/StoriesNav.tsx
@@ -14,23 +14,27 @@ const navigationItems = [
 export const StoriesNav = () => {
   const pathname = usePathname();
   return (
-    <div className="cc lg:p-0 lg:w-full mx-auto lg:max-w-800 flex flex-wrap lg:flex-row gap-10 lg:gap-26 items-center justify-center lg:justify-between rs-mb-5">
+    <nav
+      aria-label="stories-nav"
+      className="cc lg:p-0 lg:w-full mx-auto lg:max-w-800 flex flex-wrap lg:flex-row gap-10 lg:gap-26 items-center justify-center lg:justify-between rs-mb-5"
+    >
       <Text mb="0">View</Text>
-      <div className="flex flex-wrap lg:flex-row items-center justify-center lg:justify-between gap-10">
+      <ul className="list-none flex flex-wrap lg:flex-row items-center justify-center lg:justify-between gap-10 p-0">
         {navigationItems.map((item, key) => (
-          <Button
-            hideArrow
-            key={key}
-            href={item.href}
-            isLight
-            className="py-8 px-16 lg:rs-px-1 lg:rs-py-0"
-            solid={pathname === item.href}
-            disabled={pathname === item.href}
-          >
-            {item.label}
-          </Button>
+          <li key={key} className="m-0">
+            <Button
+              hideArrow
+              href={item.href}
+              isLight
+              className="py-8 px-16 lg:rs-px-1 lg:rs-py-0"
+              solid={pathname === item.href}
+              disabled={pathname === item.href}
+            >
+              {item.label}
+            </Button>
+          </li>
         ))}
-      </div>
-    </div>
+      </ul>
+    </nav>
   );
 };
