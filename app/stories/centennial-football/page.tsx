89151869dcf662caa640ca6f008a51bec6c783b9
rebeccahongsf <rebeccahongsf@gmail.com>
2025-10-08 18:36:21 -0700

    update preview image and social, update title

--- app/stories/centennial-football/page.tsx
+++ app/stories/centennial-football/page.tsx
@@ -11,7 +11,7 @@ import { H1 } from "@/components/Typography/Header";
 import { storiesSource } from "@/utilities/storiesSource";
 
 export const metadata: Metadata = {
-  title: "Stanford Football Highlights | 100 Years of Stanford Engineering",
+  title: "Centennial Football | 100 Years of Stanford Engineering",
   description: "",
   keywords: [
     "Stanford Engineering",
@@ -25,21 +25,21 @@ export const metadata: Metadata = {
     "Jonathan Levin",
   ],
   openGraph: {
-    title: "Stanford Football Highlights | 100 Years of Stanford Engineering",
+    title: "Centennial Football | 100 Years of Stanford Engineering",
     description: "",
     images: [
       {
-        url: "https://res.cloudinary.com/duv7bozlj/image/upload/v1748471475/100thCentennial_cakeimage_uvzm3z.png",
+        url: "https://res.cloudinary.com/duv7bozlj/image/upload/v1759973730/100th_Centennial_Tailgate_xzkl1x.jpg",
         alt: "",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Stanford Football Highlights | 100 Years of Stanford Engineering",
+    title: "Centennial Football | 100 Years of Stanford Engineering",
     description: "",
     images: [
-      "https://res.cloudinary.com/duv7bozlj/image/upload/v1748471475/100thCentennial_cakeimage_uvzm3z.png",
+      "https://res.cloudinary.com/duv7bozlj/image/upload/v1759973730/100th_Centennial_Tailgate_xzkl1x.jpg",
     ],
   },
 };
@@ -58,14 +58,14 @@ export default function page() {
         <article>
           <div className="cc bg-cardinal-red text-white">
             <H1 className="m-0 rs-pt-8 rs-pb-1 flex flex-col text-center">
-              Stanford Football Highlights
+              Centennial Football
             </H1>
           </div>
           <EmbedMediaHero
             autoplay
             isPreview
-            previewImageSrc="https://res.cloudinary.com/duv7bozlj/image/upload/v1748471475/100thCentennial_cakeimage_uvzm3z.png"
-            title="Stanford Football Highlights"
+            previewImageSrc="https://res.cloudinary.com/duv7bozlj/image/upload/v1759973730/100th_Centennial_Tailgate_xzkl1x.jpg"
+            title="Centennial Football"
             className="bg-cardinal-red"
             mediaUrl="https://www.youtube.com/embed/nsxuADCSTYg?si=fcS-9_GRgC9wsqtV"
           />
